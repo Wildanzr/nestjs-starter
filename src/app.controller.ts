e0cd745a4f310415822883e4ce26680b66f81b77
wildanzr <graita.sukma@gmail.com>
2023-09-09 20:09:50 +0700

    feat: base config for nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+
+class Test {
+  @ApiProperty({ required: true, type: String, description: 'Name' })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({ required: true, type: Number, description: 'Age' })
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('test')
+  getTest(@Body() body: Test): Test {
+    return body;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    logger: ['error', 'debug', 'log', 'verbose', 'warn'],
+  });
+
+  // ValidationPipe
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
+  }))
+
+  // CORS
+  const corsOptions: CorsOptions = {
+    origin: process.env.NODE_ENV === 'production' ?
+      ['*'] :
+      '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  }
+  app.enableCors(corsOptions);
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJs API')
+    .setDescription('NestJs API description')
+    .setVersion('1.0')
+    .addTag('NestJs')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
+    .build()
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  // Helmet
+  app.use(helmet())
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 bootstrap();
